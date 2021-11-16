0359c19ede58880f8c79670c2c7281757f5c4c41
TomasBabkineDiCaprio <babkinedicapriotomas@gmail.com>
2021-11-16 13:29:03 -0500

    Final stories

--- src/components/stories-container/index.jsx
+++ src/components/stories-container/index.jsx
@@ -1,12 +1,42 @@
 import "./styles.css";
+import { useEffect, useState } from 'react';
 
+// Components
+import { Story } from "../story";
+
+// Import data from "database"
+import userData from "./../../databases/users.json"
 
 export const StoriesContainer = () => {
 
-    // Data from the "database"
-    const data = require("../../databases/users.json");
-    console.log(data);
+    // Users state
+    const [users, setUsers] = useState([]);
 
-    
+    // Get all of the users, for the sake of simplicity, we're going to assume that all users have posted a story recently
+    useEffect(
+      () => {
+        getUsers();
+      }, []
+    );
+
+    const getUsers = () => {
+        setUsers(userData);
+    }
+
+    users.map( (user) => {
+        console.log(user.profile_picture);
+    })
+
+    return (
+        <div className="storiesContainer">
+            <ul>
+                {
+                    users.map( (user) => (
+                        <Story id={user.id} profilePicture={user.profile_picture} handle={user.handle}></Story>
+                    ))
+                }
+            </ul>
+        </div>
+    );
 
 }
